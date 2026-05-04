322ebcd338e85a54f57e0bbea2948c309b6544f5
uputus <markusvilisalu@gmail.com>
2026-05-05 02:47:22 +0300

    Details view.controller.idexses

---
+++ TechnoCinema/Controllers/MoviesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TechnoCinema.Data;
+using TechnoCinema.Models;
+
+namespace TechnoCinema.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Movie List
+        public async Task<IActionResult> Index()
+        {
+            var movies = await _context.Movies.ToListAsync();
+            return View(movies);
+        }
+
+        // Movie Details
+        public async Task<IActionResult> Details(int id)
+        {
+            var movie = await _context.Movies
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (movie == null)
+                return NotFound();
+
+            var showtimes = await _context.Showtimes
+                .Where(s => s.Film == movie.Title)
+                .OrderBy(s => s.ReleaseDate)
+                .ToListAsync();
+
+            ViewBag.Showtimes = showtimes;
+
+            return View(movie);
+        }
+    }
+}
